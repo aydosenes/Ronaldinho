11481a12ac63e2d67096035a94c56aea567b59e0
Enes Aydos <aydosenes@outlook.com>
2020-02-16 22:47:05 +0300

    init

---
+++ src/main/java/com/aydos/ronaldinho/RonaldinhoGauchoApplication.java
@@ -0,0 +1,13 @@
+package com.aydos.ronaldinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RonaldinhoGauchoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RonaldinhoGauchoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aydos/ronaldinho/RonaldinhoGauchoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aydos.ronaldinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RonaldinhoGauchoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
